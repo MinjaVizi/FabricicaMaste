f6ebf0a4978b85260f25b311dad5ce59cc6cc995
MinjaVizi <viziminja1509@gmail.com>
2023-11-18 03:34:54 +0100

    izmene

--- css/JS/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var hamburger = document.querySelector('.hamburger-menu');
-    var nav = document.querySelector('header nav');})
-
-    hamburger.addEventListener('click', function() {
-        nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-    }); 
